2b331b439502bd9c4d05e4555b179324b524dd6d
Айсулу <aisuluueco2009@yandex.ru>
2019-09-27 15:09:30 +0600

    Added files in 'static' folder for style, created page 'index.html'in article folder.

--- source/main/urls.py
+++ source/main/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp.views import ArticleIndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', ArticleIndexView.as_view(), name='index'),
 ]
