32347bad4124a127965aa712ef50bd083a9a6c04
Айсулу <aisuluueco2009@yandex.ru>
2019-09-27 15:17:01 +0600

    Created pade 'article.html' in folder 'article'.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -12,4 +12,15 @@ class ArticleIndexView(TemplateView):
         return context
 
 
+class ArticleView(TemplateView):
+    template_name = 'article/article.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        article_pk = kwargs.get('pk')
+        context['article'] = get_object_or_404(Article, pk=article_pk)
+        return context
+
+
+
 
