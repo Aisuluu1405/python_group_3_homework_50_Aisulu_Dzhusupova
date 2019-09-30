6ad0ffc91cd9cc87d457e157e67f751a33cd2ae1
Айсулу <aisuluueco2009@yandex.ru>
2019-09-30 19:54:36 +0600

    Разделили views по папкам 'article_views.py' и 'comment_views.py'

---
+++ source/webapp/views/__init__.py
@@ -0,0 +1,6 @@
+from .article_views import  ArticleIndexView, ArticleView,\
+        ArticleCreateView, ArticleEditView, ArticleDeleteView,\
+        ArticleCommentView, ArticleCommentCreateView
+
+from .comment_views import CommentIndexView, CommentCreateView, CommentEditView, CommentDeleteView
+
--- source/webapp/views/article_views.py
+++ source/webapp/views/article_views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.views.generic import TemplateView
 from django.views import View
-from webapp.models import Article, Comment
+from webapp.models import Article
 
-from webapp.forms import ArticleForm, CommentForm
+from webapp.forms import ArticleForm
 
 
 class ArticleIndexView(TemplateView):
@@ -111,73 +111,6 @@ class ArticleDeleteView(View):
         return redirect('index')
 
 
-class CommentIndexView(TemplateView):
-    template_name = 'comments/index.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['comments'] = Comment.objects.all().order_by('-created_at')
-        return context
-
-
-class CommentCreateView(FormView):
-    template_name = 'comments/create.html'
-
-    def get_form(self, data=None):
-        return CommentForm(data=data)
-
-    def form_valid(self, form):
-        data = form.cleaned_data
-        self.comment = Comment.objects.create(
-            article=data['article'],
-            text=data['text'],
-            author=data['author'],
-        )
-
-    def get_url(self):
-        return reverse('comment_index')
-
-
-class CommentEditView(View):
-
-    def get(self, request, *args, **kwargs):
-        comment_pk = kwargs.get('pk')
-        comment = get_object_or_404(Comment, pk=comment_pk)
-        form = CommentForm(data={
-            'article': comment.article_id,
-            'text': comment.text,
-            'author': comment.author
-            })
-        return render(request, 'comments/update.html', context={'form': form, 'comment': comment})
-
-    def post(self, request, *args, **kwargs):
-        form = CommentForm(data=request.POST)
-        comment_pk = kwargs.get('pk')
-        comment = get_object_or_404(Comment, pk=comment_pk)
-        if form.is_valid():
-            data = form.cleaned_data
-            comment.text = data['text']
-            comment.author = data['author']
-            comment.save()
-            return redirect('comment_index')
-        else:
-            return render(request, 'comments/update.html', context={'form': form, 'comment': comment})
-
-
-class CommentDeleteView(View):
-
-    def get(self, request, *args, **kwargs):
-        comment_pk = kwargs.get('pk')
-        comment = get_object_or_404(Comment, pk=comment_pk)
-        return render(request, 'comments/delete.html', context={'comment': comment})
-
-    def post(self, request, *args, **kwargs):
-        comment_pk = kwargs.get('pk')
-        comment = get_object_or_404(Comment, pk=comment_pk)
-        comment.delete()
-        return redirect('comment_index')
-
-
 class ArticleCommentView(TemplateView):
     template_name = 'article_comments/article_comments.html'
 
@@ -186,7 +119,6 @@ class ArticleCommentView(TemplateView):
         article_pk = kwargs.get('pk')
         context['article'] = get_object_or_404(Article, pk=article_pk)
         context['comments'] = context['article'].comments.order_by('-created_at')
-
         return context
 
 class ArticleCommentCreateView(View):
